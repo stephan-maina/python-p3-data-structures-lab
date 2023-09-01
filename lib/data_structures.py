5b44e30c8ebd2e31b4839fd1667cd13fa97db132
Mr.Perfect 092 <135037455+stephan-maina@users.noreply.github.com>
2023-09-02 01:59:25 +0300

    Update data_structures.py

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -17,22 +17,54 @@ spicy_foods = [
 ]
 
 def get_names(spicy_foods):
-    pass
+    names = [food['name'] for food in spicy_foods]
+    return names
 
 def get_spiciest_foods(spicy_foods):
-    pass
+    spiciest_food = [food for food in spicy_foods if food["heat_level"] > 5]
+    return spiciest_food
 
 def print_spicy_foods(spicy_foods):
-    pass
+    for food in spicy_foods:
+        name = food['name']
+        heat_level = '🌶' * food['heat_level']
+        print(f"{name} | Heat Level: {heat_level}")
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    pass
-
-def print_spiciest_foods(spicy_foods):
-    pass
+    cuisine_foods = {food["name"]: food for food in spicy_foods if food["cuisine"] == cuisine}
+    return cuisine_foods
 
 def get_average_heat_level(spicy_foods):
-    pass
+    heat_level_sum = sum(food["heat_level"] for food in spicy_foods)
+    average = heat_level_sum / len(spicy_foods)
+    return average
 
-def create_spicy_food(spicy_foods, spicy_food):
-    pass
+def create_spicy_food(spicy_foods, new_food):
+    spicy_foods.append(new_food)
+    return spicy_foods
+
+# Test the functions
+if __name__ == "__main__":
+    print("Names of Spicy Foods:")
+    names = get_names(spicy_foods)
+    print(names)
+
+    print("\nSpiciest Foods:")
+    spiciest = get_spiciest_foods(spicy_foods)
+    print_spicy_foods(spiciest)
+
+    print("\nSpicy Foods by Cuisine (Thai):")
+    thai_foods = get_spicy_food_by_cuisine(spicy_foods, "Thai")
+    print_spicy_foods(thai_foods.values())
+
+    print("\nAverage Heat Level:", get_average_heat_level(spicy_foods))
+
+    new_food = {
+        "name": "Spicy Ramen",
+        "cuisine": "Japanese",
+        "heat_level": 7,
+    }
+    spicy_foods = create_spicy_food(spicy_foods, new_food)
+    print("\nUpdated Spicy Foods:")
+    print_spicy_foods(spicy_foods)
+
